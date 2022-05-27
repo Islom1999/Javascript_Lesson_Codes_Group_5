4d37964ffb4512ad036a5f0af202938ff685f0e0
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-05-27 19:17:53 +0500

    lesson 33 loded

---
+++ 33-dars/script.js
@@ -0,0 +1,153 @@
+
+/*
+let str = 'Salom \n dunyo'
+console.log(str)
+*/
+
+/*
+let str = 'O\'zbekiston'
+console.log(str)
+*/
+/*
+let str = 'Salom \t dunyo'
+console.log(str)
+*/
+
+/*
+let str = 'Salom \: dunyo'
+console.log(str)
+*/
+/*
+let name = 'Nurbek'
+console.log(`Ism: ${name}`)
+*/
+/*
+console.log(`${65+35}`)
+console.log(`65+35`)
+*/
+/*
+let name = 'Ozodbek'
+console.log(name[0],name[1],name[2])
+*/
+/*
+let name = 'Ozodbek'
+console.log(name[0])
+*/
+
+/*
+let name = 'Ozodbek'
+console.log(name.length)
+*/
+/*
+let name = 'Ozodbek'
+console.log(name.toUpperCase())
+*/
+/*
+let name = 'OZODBEK'
+console.log(name.toLowerCase())
+*/
+/*
+let bool = false
+let bool1 = true
+let bool2 = 5 > 2
+let bool3 = Boolean(0)
+let bool4 = Boolean(1)
+let bool5 = Boolean(-1)
+let bool6 = Boolean('sdfdsfd')
+let bool7 = Boolean('')
+let bool8 = Boolean(null)
+let bool9 = Boolean(undefined)
+
+console.log(bool9)
+*/
+/*
+let name;
+let newName = name || 'Islom'
+
+console.log(newName)
+*/
+/*
+let name = "Ozodbek";
+let newName = name || 'Islom'
+
+console.log(newName)
+*/
+/*
+let number = 'Ozodbek'
+if(number == 'Ozodbek'){
+    console.log('Beramiz')
+}*/
+/*
+let number = 'Ozodbek'
+if(number == 'Ozodbek'){
+    console.log('Beramiz')
+}else{
+    console.log('Hech Narsa Bermaymiz')
+}*/
+/*
+let number = 10
+if(number % 2 == 0){
+    console.log('Son juft')
+}*/
+
+/*
+let number = 10
+if(number % 2 == 0){
+    console.log('Son juft')
+}else{
+    console.log('Son Toq')
+}
+*/
+/*
+let number = -6
+if(number > 0){
+    console.log('Son musbat')
+}else if(number == 0){
+    console.log('Son manfiy ham musbat ham emas son nol')
+}else{
+    console.log('Son manfiy')
+}*/
+/*
+let number = prompt('Son Kiriting')
+console.log(number, typeof number)
+*/
+/*
+let number = +prompt('Son Kiriting')
+
+if(number > 0){
+    console.log('Son musbat')
+}else if(number == 0){
+    console.log('Son manfiy ham musbat ham emas son nol')
+}else if(number < 0){
+    console.log('Son manfiy')
+}else{
+    console.log('Siz Son Kiritmadingiz')
+}
+*/
+/*
+if(true){
+    console.log('Salom')
+}else{
+    console.log('Voleykum')
+}
+*/
+/*
+if(false){
+    console.log('Salom')
+}else{
+    console.log('Voleykum')
+}
+*/
+
+/*
+let number = 1
+
+number % 2 == 0 ? console.log('Son juft'): console.log('Son toq')
+*/
+// 2-masala
+let a = 6
+let b = 6
+
+if(a > 0 && b > 0 ){
+    console.log('togri')
+}
