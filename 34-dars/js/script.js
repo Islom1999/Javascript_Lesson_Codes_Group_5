277387a532b92934d1b1da450c58b308c4b3809d
tizer7417 <tizer7417@gmail.com>
2022-05-30 23:01:30 +0500

    lesson 34 loaded

---
+++ 34-dars/js/script.js
@@ -0,0 +1,74 @@
+/*
+let kun = Math.ceil(Math.random() * 7)
+
+console.log(kun)
+
+/*
+if(kun == 1){
+    console.log('Dushanba')
+}else if(kun == 2){
+    console.log('Seshanba')
+}else if(kun == 3){
+    console.log('chorshanba')
+}else if(kun == 4){
+    console.log('payshanba')
+}else if(kun == 5){
+    console.log('juma')
+}else if(kun == 6){
+    console.log('shanba')
+}else if(kun == 7){
+    console.log('yakshanba')
+}*/
+
+/*
+switch (kun){
+    case 1: console.log('Dushanba'); break;
+    case 2: console.log('Seshanba'); break;
+    case 3: console.log('chorshanba'); break;
+    case 4: console.log('payshanba'); break;
+    case 5: console.log('juma'); break;
+    case 6: console.log('shanba'); break;
+    case 7: console.log('yakshanba'); break;
+    default: console.log('xato raqam kiritilgan')
+}
+*/
+
+/*
+let a = +prompt('a soni kirit')
+let b = +prompt('b soni kirit')
+let c = prompt('c amal kirit') // +,-,*,/,%,**
+
+switch(c){
+    case '+': console.log(a+b); break;
+    case '-': console.log(a-b); break;
+    case '*': console.log(a*b); break;
+    case '/': console.log(a/b); break;
+}
+*/
+
+/*
+console.log(1)
+console.log(2)
+console.log(3)
+// ...
+console.log(100)
+*/
+/*
+for(let i = 1; i <= n; i++){
+    console.log(i)
+}
+*/
+
+// s = 1/1 + 1/2 +1/3 ... 1/100
+// 1.2.3.4.5
+
+/*
+let n = 100
+let s = 0
+for(let i = 1; i <= n; i++){
+    s += 1/i
+}
+console.log(s)
+*/
+
+
